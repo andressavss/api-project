985e57ec19c9c2b15b427d32931e9d28a1fdf5f2
Andressa Santos <andressavieiradsantos@gmail.com>
2023-07-25 16:58:28 -0300

    Update

--- APIProject/Models/Product.cs
+++ APIProject/Models/Product.cs
@@ -1,13 +1,19 @@
-﻿namespace APIProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APIProject.Models
 {
     public class Product
     {
         public int ProductId { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
         public decimal Price { get; set; }
         public DateTime RegistrationDate { get; set; }
         public int DepartmentId { get; set; }
-        public Department Departments { get; set; }
+        public Department? Department { get; set; }
     }
 }
